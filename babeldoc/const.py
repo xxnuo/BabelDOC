f64e79342d249a9f8f0392fe07dbe5731505f181
Kerry <54252779+xxnuo@users.noreply.github.com>
2025-02-17 01:57:39 +0800

    patch: 缓存路径

--- babeldoc/const.py
+++ babeldoc/const.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+import os
+import sys
 
-CACHE_FOLDER = Path.home() / ".cache" / "babeldoc"
+ROOT_DIR: str = os.path.abspath(os.getenv("ROOT_DIR", os.path.dirname(sys.argv[0])))
+
+CACHE_FOLDER = os.path.join(ROOT_DIR, "data/cache")
 
 
 def get_cache_file_path(filename):
